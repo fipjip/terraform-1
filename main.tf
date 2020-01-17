88dac9282a684afcfca668f3ff303cea136d731c
mf <mf@example.com>
2020-01-17 05:22:06 +0000

    updated main

--- main.tf
+++ main.tf
@@ -3,11 +3,30 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c91f97cadcc8499e"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c91f97cadcc8499e"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!bin/bash
+              echo "Hello World" > /var/www/index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
 
   tags = {
     Name = "terraform-example"
 
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
